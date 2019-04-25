64bff39e511cb1fec4b69e4985408339a249b0e0
TensorTom <tomashley@protonmail.com>
2019-04-25 00:55:27 -0400

    Update colabctl.py

--- colabctl.py
+++ colabctl.py
@@ -5,6 +5,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import pickle
 import time
 
@@ -17,14 +20,22 @@ def sleep(seconds):
             continue
 
 
+def exists_by_text2(driver, text):
+    try:
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH,"//*[contains(text(), '"+str(text)+"')]")))
+    except Exception:
+        return False
+    return True
+
+
 def exists_by_text(driver, text):
-    driver.implicitly_wait(0)
+    driver.implicitly_wait(2)
     try:
         driver.find_element_by_xpath("//*[contains(text(), '"+str(text)+"')]")
     except NoSuchElementException:
-        driver.implicitly_wait(30)
+        driver.implicitly_wait(5)
         return False
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(5)
     return True
 
 
@@ -34,8 +45,34 @@ def user_logged_in(driver):
     except NoSuchElementException:
         driver.implicitly_wait(5)
         return False
-    driver.implicitly_wait(30)
+    driver.implicitly_wait(5)
     return True
+
+
+def wait_for_xpath(driver, x):
+    while True:
+        try:
+            driver.find_element_by_xpath(x)
+            return True
+        except:
+            time.sleep(0.1)
+            pass
+
+
+def scroll_to_bottom(driver):
+    SCROLL_PAUSE_TIME = 0.5
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
 fork = sys.argv[1]
 timeout = int(sys.argv[2])
@@ -66,40 +103,49 @@ if exists_by_text(wd, "Sign in"):
     chrome_options_gui.add_argument('--disable-infobars')
     wd = webdriver.Chrome('chromedriver', options=chrome_options_gui)
     wd.get("https://accounts.google.com/signin")
-    while not exists_by_text(wd, "Welcome,"):
-        pass
+    wait_for_xpath(wd, '//*[@id="yDmH0d"]/c-wiz/div/div[2]/c-wiz/c-wiz/div/div[4]/div/div/header/div[2]')
     print("Login detected. Saving cookie & restarting connection.")
     pickle.dump(wd.get_cookies(), open("gCookies.pkl", "wb"))
     wd.close()
     wd.quit()
     wd = webdriver.Chrome('chromedriver', options=chrome_options)
 
-print("Now ready for business.") # for debugging
+wd.get(colab_url)
+print("Logged in.") # for debugging
 running = False
+wait_for_xpath(wd, '//*[@id="file-menu-button"]/div/div/div[1]')
+print('Notebook loaded.')
+
 while not exists_by_text(wd, "Sign in"):
     if exists_by_text(wd, "Runtime disconnected"):
         try:
             wd.find_element_by_xpath('//*[@id="ok"]').click()
         except NoSuchElementException:
             pass
-    if exists_by_text(wd, "Notebook loading error"):
+    if exists_by_text2(wd, "Notebook loading error"):
         wd.get(colab_url)
     try:
         wd.find_element_by_xpath('//*[@id="file-menu-button"]/div/div/div[1]')
         if not running:
+            wd.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.SHIFT + "q")
             wd.find_element_by_tag_name('body').send_keys(Keys.CONTROL + Keys.F9)
             running = True
-            sleep(2)
     except NoSuchElementException:
         pass
     if running:
-        if exists_by_text(wd, fork):
-            running = False
-            print("Completion string found. Waiting for next cycle.")
-            sleep(timeout)
-        else:
-            try:
-                actions = ActionChains(wd)
-                actions.send_keys(Keys.SPACE).perform()
-            except Exception:
-                pass
+        try:
+            wd.find_element_by_css_selector('.notebook-content-background').click()
+            #actions = ActionChains(wd)
+            #actions.send_keys(Keys.SPACE).perform()
+            scroll_to_bottom(wd)
+            print("performed scroll")
+        except:
+            pass
+        for frame in wd.find_elements_by_tag_name('iframe'):
+            wd.switch_to.frame(frame)
+            for output in wd.find_elements_by_tag_name('pre'):
+                if fork in output.text:
+                    running = False
+                    print("Completion string found. Waiting for next cycle.")
+                    sleep(timeout)
+            wd.switch_to.default_content()            
